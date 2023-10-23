86763e3d6b5f60475f0a22a090159f68698b762a
elith95 <60621505+elith95@users.noreply.github.com>
2023-10-23 11:21:46 +0200

    Update im.plotRGB.R

--- R/im.plotRGB.R
+++ R/im.plotRGB.R
@@ -1,5 +1,5 @@
 # im.plotRGB: plotRGB choosing bands
-
+#Aggiungendo i valori di default per le bande r, g e b si possono unificare im.plotRGB e im.plotRGB.auto
 im.plotRGB <- function(x,r,g,b){
   plotRGB(x,r,g,b,stretch="lin")
 }
