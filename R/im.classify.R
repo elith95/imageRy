2005e9aa29da6b9e3ac68b66bc8a01dcd7a2ca10
elith95 <60621505+elith95@users.noreply.github.com>
2023-11-15 10:41:09 +0100

    Update im.classify.R
    Modifica funzione

--- R/im.classify.R
+++ R/im.classify.R
@@ -1,12 +1,12 @@
-im.classify <- function(input_image, num_clusters = 3, seed = NULL, do_plot = TRUE, use_viridis = TRUE, colors = c('yellow','black','red'), num_colors = 100) {
+im.classify <- function(input_image, num_clusters = 3, seed = NULL) {
   
   # Check if input is a SpatRaster
   if(!inherits(input_image, "SpatRaster")) {
     stop("input_image should be a SpatRaster object.")
   }
   
-  # Get the raster values and remove NA values
-  image_values <- na.omit(values(input_image))
+  # Get the raster values 
+  df_r <- as.data.frame(input_image, xy = T)
   
   # Set seed if provided
   if (!is.null(seed)) {
@@ -14,23 +14,13 @@ im.classify <- function(input_image, num_clusters = 3, seed = NULL, do_plot = TR
   }
   
   # Classify using kmeans
-  kmeans_result <- kmeans(image_values, centers = num_clusters)
+  kmeans_res <- kmeans(df_r[,3:ncol(df_r)], centers = num_clusters)
   
   # Set values to a raster based on kmeans result
-  classified_image <- input_image
-  values(classified_image) <- kmeans_result$cluster
-  
-  # Plot if required
-#   if (do_plot) {
-#     if (use_viridis) {
-#       color_palette <- viridis(num_colors)
-#     } else {
-#       color_palette <- colorRampPalette(colors)(num_colors)
-#     }
-#     plot(classified_image, col = color_palette, axes = FALSE)
-#   }
+  classified_image <- rast(data.frame(df_r[,1:2], kmeans_res$cluster))
   
   return(classified_image)
 }
+
 
 
